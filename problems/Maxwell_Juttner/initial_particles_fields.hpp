9b0c7102fe84ffa58bbbc38e21e8807d55c41fe5
PJD <pranab.deka@kuleuven.be>
2026-03-30 20:22:03 +0200

    Restructure functions for restarting data

--- problems/Maxwell_Juttner/initial_particles_fields.hpp
+++ problems/Maxwell_Juttner/initial_particles_fields.hpp
@@ -116,45 +116,9 @@ void EMfields3D::init_Maxwell_Juttner()
         for (int is = 0; is < ns; is++)
             grid->interpN2C(rhocs, is, rhons);
     }
+    
     else
-    {   
-        //! READ FROM RESTART
-        #ifdef NO_HDF5
-            eprintf("restart requires compiling with HDF5");
-        #else
-            
-            col->read_field_restart(vct, grid, Bxn, Byn, Bzn, Bxc, Byc, Bzc, Ex, Ey, Ez, rhoc_avg, divE_average, ns);
-
-            //* Communicate ghost data for B at cell centres
-            communicateCenterBC(nxc, nyc, nzc, Bxc, col->bcBx[0],col->bcBx[1],col->bcBx[2],col->bcBx[3],col->bcBx[4],col->bcBx[5], vct,this);
-            communicateCenterBC(nxc, nyc, nzc, Byc, col->bcBy[0],col->bcBy[1],col->bcBy[2],col->bcBy[3],col->bcBy[4],col->bcBy[5], vct,this);
-            communicateCenterBC(nxc, nyc, nzc, Bzc, col->bcBz[0],col->bcBz[1],col->bcBz[2],col->bcBz[3],col->bcBz[4],col->bcBz[5], vct,this);
-
-            //* Communicate ghost data for rhoc_avg at cell centres
-            communicateCenterBC(nxc, nyc, nzc, rhoc_avg, col->bcBx[0],col->bcBx[1],col->bcBx[2],col->bcBx[3],col->bcBx[4],col->bcBx[5], vct,this);
-
-            //* Communicate ghost data for B on nodes
-            communicateNodeBC_old(nxn, nyn, nzn, Bxn, col->bcBx[0],col->bcBx[1],col->bcBx[2],col->bcBx[3],col->bcBx[4],col->bcBx[5], vct, this);
-            communicateNodeBC_old(nxn, nyn, nzn, Byn, col->bcBy[0],col->bcBy[1],col->bcBy[2],col->bcBy[3],col->bcBy[4],col->bcBy[5], vct, this);
-            communicateNodeBC_old(nxn, nyn, nzn, Bzn, col->bcBz[0],col->bcBz[1],col->bcBz[2],col->bcBz[3],col->bcBz[4],col->bcBz[5], vct, this);
-
-            //* Communicate ghost data for E on nodes
-            communicateNodeBC_old(nxn, nyn, nzn, Ex, col->bcEx[0],col->bcEx[1],col->bcEx[2],col->bcEx[3],col->bcEx[4],col->bcEx[5], vct, this);
-            communicateNodeBC_old(nxn, nyn, nzn, Ey, col->bcEy[0],col->bcEy[1],col->bcEy[2],col->bcEy[3],col->bcEy[4],col->bcEy[5], vct, this);
-            communicateNodeBC_old(nxn, nyn, nzn, Ez, col->bcEz[0],col->bcEz[1],col->bcEz[2],col->bcEz[3],col->bcEz[4],col->bcEz[5], vct, this);
-
-            //* Initialise rho at cell centers
-            // for (int is = 0; is < ns; is++)
-            //     grid->interpN2C(rhocs, is, rhons);
-
-            if (vct->getCartesian_rank() == 0)
-            {
-                cout << "--------------------------------------------------------" << endl;
-                cout << "SUCCESSFULLY READ FIELD DATA FROM HDF5 FILES FOR RESTART" << endl;
-            }
-        
-        #endif // NO_HDF5
-    }
+        init_fields_restart();
 }
 
 //? ========================================================================== ?//
